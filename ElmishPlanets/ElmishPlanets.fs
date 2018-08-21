be6bf09874236b4b0481588f11b90a92aeb87543
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-08-21 22:29:15 +0200

    Now with a spinning 3D Pyramid!

--- ElmishPlanets/ElmishPlanets.fs
+++ ElmishPlanets/ElmishPlanets.fs
@@ -4,23 +4,22 @@ open Cmd
 open Elmish.XamarinForms
 open Elmish.XamarinForms.DynamicViews
 open Xamarin.Forms
-open Urho
 
 module App =
     let mutable _app: HelloWorldUrhoApp option = None
 
     type Model = { ShowSurface: bool }
 
-    type Msg = Appearing | Created of HelloWorldUrhoApp | ChangeColor
+    type Msg = Appearing | Created of HelloWorldUrhoApp | ToggleAnimations
 
     let setApp app =
         _app <- Some app
         None
 
-    let changeTextColor() =
+    let toggleAnimations() =
         match _app with
         | None -> ()
-        | Some app -> app.ChangeTextColor()
+        | Some app -> app.ToggleActions()
         None
 
     let init () = { ShowSurface = false }, Cmd.none
@@ -29,7 +28,7 @@ module App =
         match msg with
         | Appearing -> { model with ShowSurface = true }, Cmd.none
         | Created app -> model, Cmd.ofMsgOption (setApp app)
-        | ChangeColor -> model, Cmd.ofMsgOption (changeTextColor())
+        | ToggleAnimations -> model, Cmd.ofMsgOption (toggleAnimations())
 
     let view (model: Model) dispatch =
         View.ContentPage(
@@ -44,7 +43,7 @@ module App =
                                     options = { AssetsFolder = None },
                                     created = (fun app -> dispatch (Created app)),
                                     verticalOptions = LayoutOptions.FillAndExpand)
-                        yield View.Button(text = "Change color", command = (fun () -> dispatch ChangeColor))
+                        yield View.Button(text = "Toggle animations", command = (fun () -> dispatch ToggleAnimations))
                 ]
             )
         )
--- ElmishPlanets/HelloWorldUrhoApp.fs
+++ ElmishPlanets/HelloWorldUrhoApp.fs
@@ -1,35 +1,54 @@
 namespace ElmishPlanets
 
 open Urho
+open Urho.Actions
 open Urho.Gui
+
+module HelloWorldUrhoApp =
+    let create3DObject (resourceCache: Urho.Resources.ResourceCache) =
+        let scene = new Scene()
+        scene.CreateComponent<Octree>() |> ignore
+
+        let node = scene.CreateChild()
+        node.Position <- Vector3(0.f, 0.f, 3.5f)
+        node.Rotation <- Quaternion(15.f, 0.f, 0.f)
+        node.SetScale(1.f)
+
+        // Add Pyramid Model
+        let modelObject = node.CreateComponent<StaticModel>()
+        modelObject.Model <- resourceCache.GetModel("Models/Pyramid.mdl")
+
+        let light = scene.CreateChild(name = "light")
+        light.SetDirection(Vector3(0.4f, -0.5f, 0.3f))
+        light.CreateComponent<Light>()|> ignore
+
+        let cameraNode = scene.CreateChild(name = "camera")
+        let camera = cameraNode.CreateComponent<Camera>()
+
+        (scene, camera, node)
+
 
 type HelloWorldUrhoApp(options: ApplicationOptions) =
     inherit Urho.Application(options)
 
-    let createText(resourceCache: Urho.Resources.ResourceCache, textColor: Color) =
-        let text = new Text()
-        text.Value <- "Hello World!"
-        text.HorizontalAlignment <- HorizontalAlignment.Center
-        text.VerticalAlignment <- VerticalAlignment.Center
-        text.SetColor(textColor)
-        text.SetFont(font=resourceCache.GetFont("Fonts/Anonymous Pro.ttf"), size=30.f) |> ignore
-        text
-
-    let getTextColor counter =
-        match counter with
-        | 0 -> Color.Cyan
-        | 1 -> Color.Red
-        | 2 -> Color.Yellow
-        | _ -> failwith "No fourth color"
-
-    member val Label: Text = null with get, set
-    member val Counter: int = 0 with get, set
+    member val Node : Node = null with get, set
+    member val IsPaused = false with get, set
 
     override this.Start() =
         base.Start()
-        this.Label <- createText(this.ResourceCache, getTextColor this.Counter)
-        this.UI.Root.AddChild(this.Label)
 
-    member this.ChangeTextColor() =
-        this.Counter <- (this.Counter + 1) % 3
-        this.Label.SetColor(getTextColor this.Counter)
+        let (scene, camera, node) = HelloWorldUrhoApp.create3DObject this.ResourceCache
+        this.Renderer.SetViewport(0u, new Viewport(scene, camera, null))
+        this.Node <- node
+
+        let actions: FiniteTimeAction array = [| new RepeatForever(new RotateBy(1.f, 0.f, 90.f, 0.f)) |]
+        node.RunActionsAsync(actions)
+        |> Async.AwaitTask
+        |> Async.Ignore
+        |> Async.StartImmediate
+
+    member this.ToggleActions() =
+        this.IsPaused <- not this.IsPaused
+        match this.IsPaused with
+        | false -> this.Node.ResumeAllActions()
+        | true -> this.Node.PauseAllActions()
