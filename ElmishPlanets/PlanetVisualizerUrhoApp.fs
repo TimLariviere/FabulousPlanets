f78e5cc74ee473490507cedf8a466de3612cbc97
Timothé LARIVIERE <timothe.lariviere@outlook.com>
2018-09-06 21:33:46 +0200

    Refacto

--- ElmishPlanets/PlanetVisualizerUrhoApp.fs
+++ ElmishPlanets/PlanetVisualizerUrhoApp.fs
@@ -19,7 +19,8 @@ module PlanetVisualizerUrhoApp =
         | false ->
             scene.GetChild("setup_default").Enabled <- true
             scene.RemoveChild(scene.GetChild("setup_rings"))
-            scene.RemoveChild(scene.GetChild("planet").GetChild("rings"))
+            let planet = scene.GetChild("planet")
+            planet.RemoveChild(planet.GetChild("rings"))
         scene
         
     let setViewport (renderer: Renderer) (scene: Scene) =
@@ -27,7 +28,7 @@ module PlanetVisualizerUrhoApp =
         let camera = cameraNode.GetComponent<Camera>()
         let viewport = new Viewport(scene, camera, null)
         renderer.SetViewport(0u, viewport)
-        cameraNode
+        scene
                 
     let findPlanet (scene: Scene) =
         let planet = scene.GetChild("planet")
@@ -35,79 +36,110 @@ module PlanetVisualizerUrhoApp =
         let rings = planet.GetChild("rings")
         (planet, body, rings)
         
-    let setBodyMaterial (resourceCache: Urho.Resources.ResourceCache) materialPath (planet: Node, body: Node, rings: Node) =
-        let staticModel = body.GetComponent<StaticModel>()
-        staticModel.SetMaterial(resourceCache.GetMaterial(materialPath))
-        (planet, body, rings)
+    let setMaterial (resourceCache: Urho.Resources.ResourceCache) materialPath (node: Node) =
+        match node with 
+        | null -> ()
+        | _ -> node.GetComponent<StaticModel>().SetMaterial(resourceCache.GetMaterial(materialPath))
+        node
         
-    let setRingsMaterialIf hasRings (resourceCache: Urho.Resources.ResourceCache) materialPath (planet: Node, body: Node, rings: Node) =
-        match hasRings with
-        | false -> ()
-        | true ->
-            let staticModel = rings.GetComponent<StaticModel>()
-            staticModel.SetMaterial(resourceCache.GetMaterial(materialPath))
-        (planet, body, rings)
+    let setAxialTilt axialTilt (node: Node) =
+        node.Rotation <- Quaternion(0.f, 0.f, (float32 axialTilt) * -1.f)
+        node
         
-    let setAxialTilt axialTilt (planet: Node, body: Node, rings: Node) =
-        planet.Rotation <- Quaternion(0.f, 0.f, (float32 axialTilt) * -1.f)
-        (planet, body, rings)
+    let enable (node: Node) =
+        node.Enabled <- true
+        node
         
-    let enableNode hasRings (planet: Node, body: Node, rings: Node) =
-        planet.Enabled <- true
-        rings.Enabled <- hasRings
-        (planet, body, rings)
-        
-    let rotatePlanetForever rotationSpeed (planet: Node, _, _) =
+    let rotateForever rotationSpeed (node: Node) =
         let actions: FiniteTimeAction array = [| new RepeatForever(new RotateBy(1.f, 0.f, rotationSpeed, 0.f)) |]
-        planet.RunActionsAsync(actions)
+        node.RunActionsAsync(actions)
         |> Async.AwaitTask
         |> Async.Ignore
         |> Async.StartImmediate
+        
+    let computeRotationSpeed (planet: Planet) =
+        let rotationSpeedRelativeToEarth = planet.Info.RotationPeriod / 24.<h>
+        let rotationSpeedInDegrees = -22.5 / rotationSpeedRelativeToEarth
+
+        // Round rotation speed for smoother animations
+        if rotationSpeedInDegrees > 1. then
+            float32 (Math.Round(rotationSpeedInDegrees))
+        else
+            float32 rotationSpeedInDegrees
+            
+    let computeRemainingTime remainingTime deltaTime =
+        match remainingTime with 
+        | Some x when x > 0.f -> Some (x - deltaTime)
+        | _ -> None
+        
+    let resumeAnimation planet axialTilt rotationSpeed =
+        planet |> setAxialTilt axialTilt |> rotateForever rotationSpeed
+        
+    let userPan (planet: Node) (delta: IntVector2) =
+        planet.RemoveAllActions()
+        if abs(delta.X) > abs(delta.Y) then
+            planet.Rotate(Quaternion(0.f, (float32 -delta.X) / 5.f, 0.f), TransformSpace.World)
+        else
+            planet.Rotate(Quaternion((float32 -delta.Y) / 5.f, 0.f, 0.f), TransformSpace.World)
         
 open PlanetVisualizerUrhoApp
 
 type PlanetVisualizerUrhoApp(options: ApplicationOptions) =
     inherit Urho.Application(options)
     
-    member val PlanetNode: Node = null with get, set
+    let defaultWaitTime = 5.f
+    
+    let mutable planetNode: Node option = None
+    let mutable rotationSpeed = 0.f
+    let mutable axialTilt = 0.f
+    let mutable waitTimeBeforeResumeAnimation = None
 
     override this.Start() =
         base.Start()
 
     override this.OnUpdate timeStep =
-        match this.Input.NumTouches with
-        | 0u -> ()
-        | 2u ->
-            let touch = this.Input.GetTouch(0u)
-            this.PlanetNode.RemoveAllActions()
-            this.PlanetNode.Rotate(Quaternion(0.0f, 0.0f, (float32 -touch.Delta.X)), TransformSpace.World)
-        | _ ->
-            let touch = this.Input.GetTouch(0u)
-            this.PlanetNode.RemoveAllActions()
-            this.PlanetNode.Rotate(Quaternion((float32 -touch.Delta.Y), (float32 -touch.Delta.X), 0.f), TransformSpace.World)
+        match planetNode with
+        | None -> ()
+        | Some planet ->
+            match this.Input.NumTouches with
+            | 0u ->
+                // Check if we should resume the animation
+                let remainingTime = waitTimeBeforeResumeAnimation
+                match remainingTime, (computeRemainingTime remainingTime timeStep) with
+                | Some _, Some newTime ->
+                    waitTimeBeforeResumeAnimation <- Some newTime
+                | Some _, None ->
+                    waitTimeBeforeResumeAnimation <- None
+                    resumeAnimation planet axialTilt rotationSpeed
+                | _ -> ()
+                
+            | _ ->
+                // The user uses 1 finger to pan
+                waitTimeBeforeResumeAnimation <- Some defaultWaitTime
+                userPan planet (this.Input.GetTouch(0u).Delta)
 
     member this.LoadPlanet (planet: Planet) =
-        Urho.Application.InvokeOnMain(fun() ->
-            let rotationSpeedRelativeToEarth = planet.Info.RotationPeriod / 24.<h>
-            let rotationSpeedInDegrees = -22.5 / rotationSpeedRelativeToEarth
-
-            // Round rotation speed for smoother animations
-            let rotationSpeed = if rotationSpeedInDegrees > 1. then Math.Round(rotationSpeedInDegrees) else rotationSpeedInDegrees
-        
+        rotationSpeed <- computeRotationSpeed planet
+        axialTilt <- float32 planet.Info.AxialTilt
+            
+        Urho.Application.InvokeOnMain(fun() ->        
             let scene = 
                 create3DScene this.ResourceCache
                 |> removeUnusedNodes planet.Info.Rings.IsSome
-
-            let cameraNode =
-                scene
                 |> setViewport this.Renderer
 
-            scene
-            |> findPlanet
-            |> setBodyMaterial this.ResourceCache ("Materials/" + planet.Info.Name + ".xml")
-            |> setRingsMaterialIf planet.Info.Rings.IsSome this.ResourceCache ("Materials/" + planet.Info.Name + "Rings.xml")
-            |> setAxialTilt (float32 planet.Info.AxialTilt)
-            |> enableNode planet.Info.Rings.IsSome
-            |> (fun (p, b, r) -> this.PlanetNode <- p; (p, b, r))
-            |> rotatePlanetForever (float32 rotationSpeed)
+            let planet = 
+                scene
+                |> findPlanet
+                |> (fun (p, b, r) ->
+                        b |> setMaterial this.ResourceCache ("Materials/" + planet.Info.Name + ".xml") |> ignore
+                        r |> setMaterial this.ResourceCache ("Materials/" + planet.Info.Name + "Rings.xml") |> ignore
+                        p
+                    )
+                |> setAxialTilt axialTilt
+                |> enable
+                
+            planet |> rotateForever rotationSpeed
+                
+            planetNode <- Some planet
         )
